d0f5bdcf1f4edcec07a040388b58b340b3bd9a84
Cassie <>
2020-02-24 11:04:42 +0800

    init

---
+++ TestSonarCloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestSonarCloud
+//
+//  Created by Cassie on 2020/2/24.
+//  Copyright © 2020 Cassie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

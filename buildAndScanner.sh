4e8a7b717751e3547bcc144c123a08261bc8efb5
Cassie <>
2020-02-26 14:10:45 +0800

    Add sh

---
+++ buildAndScanner.sh
@@ -0,0 +1,20 @@
+rm -rf ./build
+rm -rf sonarqube-generic-coverage.xml
+
+xcodebuild test \
+             -project "TestSonarCloud.xcodeproj" \
+             -scheme "TestSonarCloud" \
+             -configuration "Debug" \
+             -derivedDataPath "./build" \
+             -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max' \
+             -enableCodeCoverage YES \
+             | xcpretty
+
+bash xccov-to-sonarqube-generic.sh build/Logs/Test/*.xcresult/ > sonarqube-generic-coverage.xml
+
+sonar-scanner -Dsonar.organization=huahuaccc \
+			  -Dsonar.projectKey=huahuaCCC_TestSonarCloud   
+			  -Dsonar.sources=.   
+			  -Dsonar.host.url=https://sonarcloud.io   
+			  -Dsonar.login=1cf4bcd8226c5906d5ac4e81709b22bb8e760be1
+

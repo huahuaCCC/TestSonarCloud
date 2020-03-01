b30f61bb94d553a8f4b406d9d0d3b40e2e71caf7
Cassie <>
2020-03-01 16:20:45 +0800

    Add code

--- TestSonarCloud/SecondViewController.swift
+++ TestSonarCloud/SecondViewController.swift
@@ -17,5 +17,13 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         detailLabel.text = "Detail Text"
         imgView.image = UIImage.init(named: "zzpic")
+        imgView.layer.borderWidth = 2
+        let color = self.getBorderColor()
+        imgView.layer.borderColor = color.cgColor
+    }
+    
+    func getBorderColor() -> UIColor {
+        let c = Int.random(in: 0..<100)
+        return c % 2 == 0 ? UIColor.red : UIColor.brown
     }
 }

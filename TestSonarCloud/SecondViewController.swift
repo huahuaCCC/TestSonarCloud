3da5594945c5a60e922d308d09e84df7430c6c6d
Cassie <>
2020-04-15 09:33:29 +0800

    Add

--- TestSonarCloud/SecondViewController.swift
+++ TestSonarCloud/SecondViewController.swift
@@ -28,7 +28,7 @@ class SecondViewController: UIViewController {
     }
     
     func getAddress() -> String {
-        return "Shanghai"
+        return "Shanghai in China"
     }
     
     func square( a : Int) -> Int {
